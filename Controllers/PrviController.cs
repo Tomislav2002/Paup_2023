d006d6b18dc8966e00ffb0af5914ee338f9b7ada
mev <mev@PC-123-2>
2023-03-13 13:19:29 +0100

    Add project files.

---
+++ Controllers/PrviController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Paup_2023.Controllers
+{
+    public class PrviController : Controller
+    {
+        // GET: Prvi
+        public ActionResult Index()
+        {
+            ViewBag.Lokacija = "Međimursko veleučilište u Čakovcu";
+            return View();
+        }
+
+        public ActionResult Druga()
+        {
+            ViewBag.Ustanova = "Međimursko veleučilište";
+            ViewData["Lokacija"] = "Čakovec";
+            return View();
+        }
+
+        public ActionResult Treca(string poruka, int broj=1)
+        {
+            ViewBag.Poruka = poruka;
+            ViewBag.Broj = broj;
+            return View();
+        }
+
+        public ActionResult Student()
+        {
+            ViewBag.Ime = "Tomislav";
+            ViewBag.Prezime = "Sklepic";
+            ViewBag.GodinaRođenja = 2002;
+            return View();
+        }
+    }
+}
